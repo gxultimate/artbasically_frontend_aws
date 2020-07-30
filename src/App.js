644aa73cc15bb683d052ac5184ee301ba0ce1eb1
ralph <alquisadaralph@gmail.com>
2020-07-30 17:45:43 +0800

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import Routes from './Routes';
 import './css/style.css';
 import './css/normalize.css';
