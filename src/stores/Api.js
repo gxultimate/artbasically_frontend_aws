4d18e4c00ce85bcc52ed490381bfdc263f039985
ralph <alquisadaralph@gmail.com>
2020-09-04 15:14:58 +0800

    added

--- src/stores/Api.js
+++ src/stores/Api.js
@@ -6,7 +6,7 @@ class Api {
     // baseURL: 'https://0390f98b29fa.ngrok.io/',
     // baseURL: "https://artbasicallymaster.herokuapp.com/"
     // baseURL: "https://gxultimate-artbasicallybackend.glitch.me/"
-    baseURL: 'http://localhost:4000/',
+    baseURL: '/api',
     // baseURL: 'http://localhost:4000/',
     // baseURL: "https://artbasically.com/server"
   });
@@ -17,113 +17,113 @@ class Api {
 
   // ACCOUNT
   addaccount = (data) => {
-    return this.api.post('/addAccounts', {
+    return this.api.post('accountsRoute/addAccounts', {
       mode: 'cors',
       data: data,
     });
   };
 
   getaccounts = () => {
-    return this.api.get('/getAccounts');
+    return this.api.get('accountsRoute/getAccounts');
   };
 
   editAccount = (data) => {
-    return this.api.post('/editAccount', {
+    return this.api.post('accountsRoute/editAccount', {
       mode: 'cors',
       data: data,
     });
   };
   addOrder = (data) => {
-    return this.api.post('/addOrder', {
+    return this.api.post('orderRoute/addOrder', {
       mode: 'cors',
       data: data,
     });
   };
 
   loginaccount = async (data) => {
-    return this.api.post('/loginAccounts', {
+    return this.api.post('accountsRoute/loginAccounts', {
       mode: 'cors',
       data: data,
     });
   };
 
   getOrder = (id) => {
-    return this.api.get(`/getOrder/${id}`, {
+    return this.api.get(`orderRoute/getOrder/${id}`, {
       mode: 'cors',
     });
   };
 
   // ARTWORK
   addartwork = (data) => {
-    return this.api.post('/addArtwork', {
+    return this.api.post('artworkRoute/addArtwork', {
       mode: 'cors',
       data: data,
     });
   };
 
   getartwork = () => {
-    return this.api.get('/getArtworks');
+    return this.api.get('artworkRoute/getArtworks');
   };
 
   getartworkInfo = () => {
-    return this.api.get('/getArtworkInfo');
+    return this.api.get('artworkRoute/getArtworkInfo');
   };
 
   getsingleartworkinfo = (id) => {
-    return this.api.get(`/getSingleArtworkInfo/${id}`);
+    return this.api.get(`artworkRoute/getSingleArtworkInfo/${id}`);
   };
 
   editArtwork = (data) => {
-    return this.api.post('/editArtwork', {
+    return this.api.post('artworkRoute/editArtwork', {
       mode: 'cors',
       data: data,
     });
   };
 
   getArtistArtwork = (id) => {
-    return this.api.get(`/getArtistArtwork/${id}`);
+    return this.api.get(`artworkRoute/getArtistArtwork/${id}`);
   };
 
   getRelatedWorkByCategory = (category) => {
-    return this.api.get(`/getRelatedWorkByCategory/${category}`);
+    return this.api.get(`artworkRoute/getRelatedWorkByCategory/${category}`);
   };
 
   // CATEGORY
   addCategory = (data) => {
-    return this.api.post('/addCategory', {
+    return this.api.post('categoryRoute/addCategory', {
       mode: 'cors',
       data: data,
     });
   };
 
   editCategory = (data) => {
-    return this.api.post('/editCategory', {
+    return this.api.post('categoryRoute/editCategory', {
       mode: 'cors',
       data: data,
     });
   };
 
   getCategories = () => {
-    return this.api.get('/getCategories');
+    return this.api.get('categoryRoute/getCategories');
   };
 
   // STYLES
   addStyle = (data) => {
-    return this.api.post('/addStyle', {
+    return this.api.post('categoryRoute/addStyle', {
       mode: 'cors',
       data: data,
     });
   };
 
   editStyle = (data) => {
-    return this.api.post('/editStyle', {
+    return this.api.post('categoryRoute/editStyle', {
       mode: 'cors',
       data: data,
     });
   };
 
   getStyles = () => {
-    return this.api.get('/getStyles');
+    return this.api.get('categoryRoute/getStyles');
   };
 
   //Print Size
@@ -145,31 +145,31 @@ class Api {
 
   //ARTIST
   getArtists = () => {
-    return this.api.get('/getArtists');
+    return this.api.get('accountsRoute/getArtists');
   };
 
   getSingleArtists = (id) => {
-    return this.api.get(`/getSingleArtists/${id}`);
+    return this.api.get(`accountsRoute/getSingleArtists/${id}`);
   };
 
   // CART
 
   addToCart = (data) => {
-    return this.api.post('/addToCart', {
+    return this.api.post('transactionRoute/addToCart', {
       mode: 'cors',
       data: data,
     });
   };
 
   getToCart = (data) => {
-    return this.api.post('/getToCart', {
+    return this.api.post('transactionRoute/getToCart', {
       mode: 'cors',
       data: data,
     });
   };
 
   editToCart = (id) => {
-    return this.api.delete(`/editToCart/${id}`, {
+    return this.api.delete(`transactionRoute/editToCart/${id}`, {
       mode: 'cors',
     });
   };
@@ -181,33 +181,33 @@ class Api {
   };
 
   getOrders = () => {
-    return this.api.get('/getAllOrders', {
+    return this.api.get('orderRoute/getAllOrders', {
       mode: 'cors',
     });
   };
 
   followAccount = (data, id) => {
-    return this.api.post(`/followAccount/${id}`, {
+    return this.api.post(`accountsRoute/followAccount/${id}`, {
       mode: 'cors',
       data: data,
     });
   };
 
   editOrder = (data) => {
-    return this.api.put('/editOrder', {
+    return this.api.put('orderRoute/editOrder', {
       mode: 'cors',
       data: data,
     });
   };
 
   getEmergingArtist = () => {
-    return this.api.get('/getEmergingArtistArtwork', {
+    return this.api.get('artworkRoute/getEmergingArtistArtwork', {
       mode: 'cors',
     });
   };
 
   getArtistFollowArtwork = (email) => {
-    return this.api.get(`/getArtistFollowArtwork/${email}`, {
+    return this.api.get(`artworkRoute/getArtistFollowArtwork/${email}`, {
       mode: 'cors',
     });
   };
